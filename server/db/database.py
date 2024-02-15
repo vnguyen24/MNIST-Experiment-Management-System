6bd0ce6057711af59da6c42919b3b98755be7e4a
Cuong <vnguyen24@amherst.edu>
2024-02-15 02:25:31 -0500

    Removed leaked secret

--- server/db/database.py
+++ server/db/database.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://cuong:6cn5c3PMmvS3178v@test.llemyrt.mongodb.net/?retryWrites=true&w=majority"
+uri = "OOPSIE!"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
