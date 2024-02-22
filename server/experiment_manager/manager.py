dc92fee5944a8fc266b0eac3fe64de19fc7cddc8
Cuong <vnguyen24@amherst.edu>
2024-02-22 00:50:11 -0500

    Write code to update runtime and accuracy in DB when job is done; Tested multiple jobs in the queue

--- server/experiment_manager/manager.py
+++ server/experiment_manager/manager.py
@@ -6,22 +6,23 @@ from server.ml_models.mnist_model import Net, load_data, MNISTModel
 import torch.nn as nn
 import torch.optim as optim
 from flask_socketio import disconnect
+import time
 
 class Manager:
     def __init__(self, socketio):
         self.socketio = socketio
-    
+
     def start_experiment(self, hyperparams):
-        batch_size = hyperparams.get("batch_size")
-        learning_rate = hyperparams.get("learning_rate")
-        epochs = hyperparams.get("epochs")
+        batch_size = hyperparams['batch_size']
+        learning_rate = hyperparams['learning_rate']
+        epochs = hyperparams['epochs']
 
         trainloader, testloader = load_data(batch_size)
         model = Net()
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
         mnistmodel = MNISTModel()
-
+        start = time.time()
         mnistmodel.train_model(model, trainloader, criterion, optimizer, epochs, self.progress_update)
         print(f"Finished training the model")
         accuracy = mnistmodel.evaluate_model(model, testloader)
@@ -29,6 +30,9 @@ class Manager:
         print(f"Done with accuracy: {accuracy}%")
         print(f"Closing websocket to avoid memory leaks")
         self.close_connection()
+        end = time.time()
+        run_time = round(end-start, 3)
+        return (accuracy, run_time)
 
 
     def progress_update(self, progress_data):
